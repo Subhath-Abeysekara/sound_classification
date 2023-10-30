0f977a1a89f38309935dd802cf3ef5dd0755cb7f
subhath abeysekara <subhath@liveroom.dev>
2023-10-31 00:22:44 +0530

    threashold error

--- convert_using_ffmpeg.py
+++ convert_using_ffmpeg.py
@@ -1,10 +1,12 @@
+import os
 import subprocess
 
 def convert_aud_ffmpeg():
     ffmpeg_path = '/usr/bin/ffmpeg'  # The path to the ffmpeg binary
     input_file = 'uploaded.amr'
     output_file = 'uploaded.wav'
-
+    if os.path.exists(output_file):
+        os.remove(output_file)
     ffmpeg_command = f'{ffmpeg_path} -i {input_file} {output_file}'
 
     try:
